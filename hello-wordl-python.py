7d0bcbc75bd1f296b89b3cd1302cdc4915ea0ac5
[PedroPabloromerotorres](https://github.com/PedroPabloromerotorres) <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-10-29 12:00:55 -0500

    EJERCICIOS OBTENER DATOS DEL USUARIO Y CONVERTILOS

---
+++ hello-wordl-python.py
@@ -0,0 +1,23 @@
+fullname = "pedro pablo romero torres" #tipo de datos string
+
+gender = "P" #tipo de dato char
+
+age = 17 #tipo de dato int 
+
+height = 1.83 #tipo de dato dobule
+
+trainer = true #tipo de dato boolean
+
+# conjuntos 
+hobbies = ["pragarmar","coninar"] #tipo de dato array 
+
+direction = (dict) ["country": "colombia", "state", "santander"] #tipo de datos object
+
+print(f""" teacher's name: "{fullname}" """) 
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+fullname = input("Enter your full name: ")
+
+age = int(input("Enter your age: "))
+
+gender = input("Enter your possible gender: options M/F/X: ")
+
+height = float(input("Enter your height: "))
+
+print(f"""
+      hola usuario {fullname}. ¿como estas? te saluda python.
+      gracias por compartir tu edad {age}; no sabia que eres un humano de tipo {gender}.
+      """)
